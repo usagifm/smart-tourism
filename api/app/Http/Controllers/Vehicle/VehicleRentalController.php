22333851d9deba1bbd97b9ce0a7a96891abda4f0
Intern 01 <rashad.fadilah@ibm-jti.com>
2021-12-06 00:32:23 +0700

    Update VehicleRentalController.php

--- api/app/Http/Controllers/Vehicle/VehicleRentalController.php
+++ api/app/Http/Controllers/Vehicle/VehicleRentalController.php
@@ -22,10 +22,7 @@ class VehicleRentalController extends Controller
         if($checkIfOngoing){
             $ongoing = 1;
 
-            $client = new Client();
-
-            $response = $client->request('POST', 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCcFHfVyWdI8H1YG67kyUup7VRq1P_fTOE');
-
+            $response = Http::post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCcFHfVyWdI8H1YG67kyUup7VRq1P_fTOE');
             $geolocate = json_decode($response->getBody(), true);
 
             $track = new VehicleTrackHistory;
