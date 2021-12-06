6d7d7ebc69c770be4bf1b60a9d60c64cca381699
Intern 01 <rashad.fadilah@ibm-jti.com>
2021-12-06 21:59:53 +0700

    Update Kernel.php

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
@@ -46,6 +46,7 @@ class Kernel extends HttpKernel
             'throttle:api',
             \App\Http\Middleware\JsonResponseMiddleware::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class
         ],
     ];
 
