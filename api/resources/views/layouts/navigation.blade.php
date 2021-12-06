1fb6f588bab2ec696efcf6aac6fc8116ec7b1424
ackyras <ackyrasibarani@gmail.com>
2021-12-06 19:34:25 +0700

    Front end - view

--- api/resources/views/layouts/navigation.blade.php
+++ api/resources/views/layouts/navigation.blade.php
@@ -1,16 +1,17 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">Smart Tourism</div>
+        <img src="{{ asset('images/TubabaPutih.png') }}" alt="" srcset="" width="180">
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
+
+    {{-- <li class="nav-item">
+        sistem administrasi
+    </li> --}}
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item @if(request()->routeIs('home')) active @endif">
--- api/resources/views/vehicles/index.blade.php
+++ api/resources/views/vehicles/index.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.app')
 
+@section('custom_styles')
+<link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
+@endsection
+
 @section('content')
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
     <!-- DataTales Example -->
     <div class="col my-2">
-        <a href="{{ route('vehicles.create') }}" class="btn btn-primary">Tambah Kendaraan</a>
+        <a href="{{ route('vehicles.create') }}" class="btn btn-danger">Tambah Kendaraan</a>
     </div>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Vehicle Table</h6>
+            <h6 class="m-0 font-weight-bold text-danger">Vehicle Table</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>No</th>
@@ -25,7 +31,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($vehicles as $vehicle)
+                        @forelse($vehicles as $vehicle)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ $vehicle->serial_number }}</td>
@@ -42,7 +48,11 @@
                                 </form>
                             </td>
                         </tr>
-                        @endforeach
+                        @empty
+                        <tr>
+                            <td colspan="5">Tidak ada data</td>
+                        </tr>
+                        @endforelse
                     </tbody>
                 </table>
 
@@ -54,4 +64,34 @@
 
 </div>
 <!-- /.container-fluid -->
+@endsection
+
+@section('custom_scripts')
+<script src="{{ asset('js/table/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/table/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('js/table/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('js/table/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('js/table/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('js/table/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('js/table/jszip.min.js') }}"></script>
+<script src="{{ asset('js/table/pdfmake.min.js') }}"></script>
+<script src="{{ asset('js/table/vfs_fonts.js') }}"></script>
+<script src="{{ asset('js/table/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('js/table/buttons.print.min.js') }}"></script>
+<script src="{{ asset('js/table/buttons.colVis.min.js') }}"></script>
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            "order": [],
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "buttons": ['copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5', "colvis"
+            ]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
+</script>
 @endsection
