713bf252cae47f07748d19e30528030581148a05
Intern 01 <rashad.fadilah@ibm-jti.com>
2021-12-06 21:59:15 +0700

    Update Kernel.php

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Illuminate\Session\Middleware\StartSession::class
+
     ];
 
     /**
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class
         ],
 
         'api' => [
