bdfc2adef2f6fe0b43725ff784751853c3fb2c87
Intern 01 <rashad.fadilah@ibm-jti.com>
2021-12-07 10:22:33 +0700

    Update OpRentalController.php

--- api/app/Http/Controllers/Operator/OpRentalController.php
+++ api/app/Http/Controllers/Operator/OpRentalController.php
@@ -120,32 +120,32 @@ class OpRentalController extends Controller
 
 
 
-        $curl = curl_init();
-        $authKey = "key=". env('FCM_SERVER_KEY');
-        $registration_ids = [$user->fcm_registration_id];
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => '{
-                        "registration_ids": ' . $registration_ids . ',
-                        "notification": {
-                            "title": "Hore ! Pesanan sewa kendaraan anda di setujui !",
-                            "body": "Kami ingatkan bahwa jangan menggunakan kendaraan sewa diluar area peminjaman ya dan tetap berhati hati dalam berkendara"
-                        }
-                    }',
-        CURLOPT_HTTPHEADER => array(
-            "Authorization: " . $authKey,
-            "Content-Type: application/json",
-            "cache-control: no-cache"
-        ),
-        ));
+        // $curl = curl_init();
+        // $authKey = "key=". env('FCM_SERVER_KEY');
+        // $registration_ids = [$user->fcm_registration_id];
+        // curl_setopt_array($curl, array(
+        // CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+        // CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_ENCODING => "",
+        // CURLOPT_MAXREDIRS => 10,
+        // CURLOPT_TIMEOUT => 30,
+        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        // CURLOPT_CUSTOMREQUEST => "POST",
+        // CURLOPT_POSTFIELDS => '{
+        //                 "registration_ids": ' . $registration_ids . ',
+        //                 "notification": {
+        //                     "title": "Hore ! Pesanan sewa kendaraan anda di setujui !",
+        //                     "body": "Kami ingatkan bahwa jangan menggunakan kendaraan sewa diluar area peminjaman ya dan tetap berhati hati dalam berkendara"
+        //                 }
+        //             }',
+        // CURLOPT_HTTPHEADER => array(
+        //     "Authorization: " . $authKey,
+        //     "Content-Type: application/json",
+        //     "cache-control: no-cache"
+        // ),
+        // ));
 
-        curl_exec($curl);
+        // curl_exec($curl);
 
 
         return response()->json(
