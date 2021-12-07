8a632a29707c14bd21eec9401d980b47fee52956
Ezra118140171 <ezra.118140171@student.itera.ac.id>
2021-12-07 12:42:53 +0700

    chart

--- api/resources/views/statistic/index.blade.php
+++ api/resources/views/statistic/index.blade.php
@@ -9,23 +9,54 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <h1 class="h3 mb-0 text-gray-800">Analisis</h1>
     </div>
 
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    Statistik
+                    <div class="card-header">
+                        <h6 class="font-weight-bold text-danger">Grafik Keuangan</h6>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="myChart"></canvas>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- /.row -->
 
 </div>
 <!-- /.container-fluid -->
 @endsection
 
 @section('custom_scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+<script>
+    var xValues = ['senin','selasa','rabu','kamis','jumat','sabtu','minggu'];
+var yValues = [7,8,10,12,14,10,16];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
+</script>
 @endsection
