d0ac119c4809e844d7a728babbb0e728df0236ac
Intern 01 <rashad.fadilah@ibm-jti.com>
2021-12-14 07:47:04 +0700

    Update DashboardController.php

--- api/app/Http/Controllers/Web/DashboardController.php
+++ api/app/Http/Controllers/Web/DashboardController.php
@@ -2,13 +2,45 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
+use App\Models\Vehicle;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    public function __invoke()
+    public function index(Request $request)
+
     {
-        return view('dashboard.index');
+
+        if($request->query('filterByRentAreaId') != null && $request->query('filterByVehicleTypeId') == null){
+            $vehicles = Vehicle::where("rent_area_id",$request->query('filterByRentAreaId'))->with('vehiclePosition')->get();
+        }
+
+
+        if($request->query('filterByRentAreaId') == null && $request->query('filterByVehicleTypeId') != null){
+
+
+            $vehicles = Vehicle::where("vehicle_type_id",  $request->query('filterByVehicleTypeId'))->with('vehiclePosition')->get();
+
+
+        }
+
+
+        if($request->query('filterByRentAreaId') != null && $request->query('filterByVehicleTypeId') != null){
+
+
+            $vehicles = Vehicle::where("vehicle_type_id",$request->query('filterByVehicleTypeId'))->where("rent_area_id", $request->query('filterByRentAreaId'))->with('vehiclePosition')->get();
+
+
+        }
+
+
+        if($request->query('filterByRentAreaId') == null && $request->query('filterByVehicleTypeId') == null){
+            $vehicles = Vehicle::with('vehiclePosition')->get();
+        }
+
+
+
+        return view('dashboard.index', compact('vehicles'));
     }
 }
