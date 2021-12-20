0049e5eb103f07b944ebab1fe693c45254942085
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-20 22:57:55 +0700

    update route web

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\Auth\WebLoginController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\DashboardController;
 use App\Http\Controllers\Web\ManageAdminController;
 use App\Http\Controllers\Web\ManageOperatorController;
 use App\Http\Controllers\Web\ManageUserController;
 use App\Http\Controllers\Web\RentAreaController;
 use App\Http\Controllers\Web\StatisticController as WebStatisticController;
+use App\Http\Controllers\Web\User\LoginController;
+use App\Http\Controllers\Web\User\ProfileController as UserProfileController;
+use App\Http\Controllers\Web\User\RegisterController;
+use App\Http\Controllers\Web\User\VehicleController;
 use App\Http\Controllers\Web\VehicleController as WebVehicleController;
 use App\Http\Controllers\Web\VehicleTypeController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,21 +29,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
 
-Route::get('login', [WebLoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [WebLoginController::class, 'login'])->name('login.post');
-
-Route::post('logout', [WebLoginController::class, 'logout'])->name('logout');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Authentication Admin
+Route::get('admin/login', [WebLoginController::class, 'showLoginForm'])->name('login');
+Route::post('admin/login', [WebLoginController::class, 'login'])->name('login.post');
+Route::post('admin/logout', [WebLoginController::class, 'logout'])->name('logout');
 
 
-Route::middleware('auth:admin')->group(function () {
+// Authentication User
+Route::get('login',     [LoginController::class, 'showLoginForm'])->name('login.user');
+Route::post('login',     [LoginController::class, 'login'])->name('login.user.post');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout.user');
+Route::get('register',  [RegisterController::class, 'showRegistrationForm'])->name('register.user');
+Route::post('register',  [RegisterController::class, 'register'])->name('register.user.post');
+
+Route::middleware(['auth:admin', 'admin'])->group(function () {
     Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 
-    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
 
     Route::get('vehicles/{vehicle}/download',   [WebVehicleController::class, 'download'])->name('vehicles.download');
     Route::resource('vehicles', WebVehicleController::class)->except('show');
@@ -66,4 +75,14 @@ Route::middleware('auth:admin')->group(function () {
         ->parameters([
             'admin' => 'admin'
         ]);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('vehicle/rent',      [VehicleController::class, 'create'])->name('dashboard');
+    Route::post('vehicle/rent',      [VehicleController::class, 'store'])->name('vehicle.rent.post');
+    Route::get('vehicle/history',   [VehicleController::class, 'history'])->name('vehicle.history');
+    Route::get('vehicle/invoices/{invoice}',   [VehicleController::class, 'invoices'])->name('vehicle.invoice');
+
+    Route::get('user', [UserProfileController::class, 'show'])->name('profile.user.show');
+    Route::put('user', [UserProfileController::class, 'update'])->name('profile.user.update');
 });
