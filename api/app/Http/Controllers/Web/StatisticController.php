7dd56c271e0786c35588171f528514e03c3e3155
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-20 22:53:59 +0700

    update StatisticController add filtering data

--- api/app/Http/Controllers/Web/StatisticController.php
+++ api/app/Http/Controllers/Web/StatisticController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,17 +13,20 @@ class StatisticController extends Controller
     {
         abort_if(Gate::denies('show_statistic_rent_vehicle'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
+        $query = request()->query('filter', 'month');
+
         $chart_options = [
             'chart_title' => 'Total Penyewaan Kendaraan',
             'report_type' => 'group_by_date',
             'model' => 'App\Models\invoice',
             'group_by_field' => 'created_at',
-            'group_by_period' => 'day',
+            'group_by_period' => $query == 'month' ? 'day' : 'month',
             'aggregate_function' => 'count',
             'aggregate_field' => 'total_charge',
             'chart_type' => 'line',
-            'filter_period' => 'month',
-            'continuous_time' => true,
+            'filter_field' => 'created_at',
+            'filter_period' => $query == 'month' ? 'month' : 'year',
+            'chart_color' => '184, 4, 4',
         ];
 
         $chart = new LaravelChart($chart_options);
@@ -36,18 +38,21 @@ class StatisticController extends Controller
     {
         abort_if(Gate::denies('show_statistic_revenue'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
+        $query = request()->query('filter', 'month');
+
         $chart_options = [
             'chart_title' => 'Total Pendapatan',
             'name' => 'Grafik Pendapatan',
             'report_type' => 'group_by_date',
             'model' => 'App\Models\invoice',
             'group_by_field' => 'created_at',
-            'group_by_period' => 'day',
+            'group_by_period' => $query == 'month' ? 'day' : 'month',
             'aggregate_function' => 'sum',
             'aggregate_field' => 'total_charge',
             'chart_type' => 'line',
-            'filter_period' => 'month',
-            'continuous_time' => true,
+            'filter_field' => 'created_at',
+            'filter_period' => $query == 'month' ? 'month' : 'year',
+            'chart_color' => '184, 4, 4',
         ];
 
         $chart = new LaravelChart($chart_options);
