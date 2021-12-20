1d92942c2dc522e8b6c7c09c35ed49d732a78364
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-20 22:56:58 +0700

    add view history for user

---
+++ api/resources/views/user/history.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.customer')
+
+@section('title')
+    Riwayat - Smart Tourism
+@endsection
+
+@section('custom_styles')
+    <link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
+@endsection
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-danger">Riwayat Peminjaman</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th style="width: 10px">No</th>
+                                <th>Serial Number</th>
+                                <th>Biaya</th>
+                                <th>Status</th>
+                                <th>Invoice</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($invoices as $invoice)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $invoice->rental->vehicle->serial_number }}</td>
+                                    <td>Rp. {{ number_format($invoice->total_charge, 2) }}</td>
+                                    <td>{{ $invoice->is_paid == '1' ? 'Lunas' : 'Belum' }}</td>
+                                    <td><a href="{{ route('vehicle.invoice', $invoice) }}"
+                                            class="btn btn-danger btn-sm">Invoice</a>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="5">Tidak ada data</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
+
+@section('custom_scripts')
+    <script src="{{ asset('js/table/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/table/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('js/table/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('js/table/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('js/table/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('js/table/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('js/table/jszip.min.js') }}"></script>
+    <script src="{{ asset('js/table/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('js/table/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('js/table/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('js/table/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('js/table/buttons.colVis.min.js') }}"></script>
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "order": [],
+                "responsive": true,
+                "lengthChange": true,
+                "autoWidth": false,
+                "buttons": ['copyHtml5',
+                    'excelHtml5',
+                    'csvHtml5',
+                    'pdfHtml5', "colvis"
+                ]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        });
+    </script>
+@endsection
