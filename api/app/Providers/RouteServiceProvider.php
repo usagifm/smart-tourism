27375dc82cb46a90933a3cda79cac35a19535bfb
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-20 22:47:17 +0700

    update RouteServiceProvider

--- api/app/Providers/RouteServiceProvider.php
+++ api/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = 'vehicle/rent';
 
     /**
      * The controller namespace for the application.
