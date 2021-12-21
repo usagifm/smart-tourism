e9a26ce0869907109378036c688b8829efbcaba4
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-21 22:35:34 +0700

    update view dashboard user

--- api/resources/views/user/dashboard.blade.php
+++ api/resources/views/user/dashboard.blade.php
@@ -14,6 +14,75 @@
 @section('content')
     <!-- Begin Page Content -->
     <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    Menunggu</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countVehicle['waiting'] }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-pause-circle fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    Berjalan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countVehicle['ongoing'] }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-motorcycle fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    Selesai</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countVehicle['ended'] }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-check-circle fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    Dibayar</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countVehicle['paid'] }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-money-bill fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-danger">Sewa Kendaraan</h6>
@@ -23,7 +92,7 @@
                     <form action="{{ route('vehicle.rent.post') }}" class="col-4 mx-auto" method="POST">
                         @csrf
                         <div class="form-group">
-                            <label for="vehicle_id">Status</label>
+                            <label for="vehicle_id">Kendaraan</label>
                             <select name="vehicle_id" id="vehicle_id" class="form-control select2" required>
                                 @foreach ($vehicles as $id => $vehicle)
                                     <option value="{{ $id }}" {{ old('vehicle_id') == $id ? 'selected' : '' }}
@@ -52,6 +121,9 @@
                             <tr>
                                 <th style="width: 10px">No</th>
                                 <th>No Serial</th>
+                                <th>Biaya</th>
+                                <th>Waktu</th>
+                                <th>Total</th>
                                 <th>Mulai</th>
                                 <th>Selesai</th>
                                 <th>Status</th>
@@ -62,9 +134,13 @@
                                 <tr>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $rental->vehicle->serial_number }}</td>
-                                    <td>{{ $rental->date_time_start ? $rental->date_time_start->format('h:i') : '-' }}
+                                    <td>Rp. {{ number_format($rental->vehicle->fare, 2) }} / 30 <sub>menit</sub></td>
+                                    <td>{{ $rental->total_time }} <sub>menit</sub></td>
+                                    <td>Rp. {{ number_format($rental->price, 2) }}</td>
+                                    <td>{{ $rental->date_time_start ? $rental->date_time_start->format('h:i d-m-Y') : '-' }}
                                     </td>
-                                    <td>{{ $rental->date_time_end ? $rental->date_time_end->format('h:i') : '-' }}</td>
+                                    <td>{{ $rental->date_time_end ? $rental->date_time_end->format('h:i d-m-Y') : '-' }}
+                                    </td>
                                     <td>{{ ucfirst($rental->status) }}</td>
                                 </tr>
                             @empty
