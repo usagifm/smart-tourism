5ae050d0b07ed1e8ec08ed9dffc6c9ae88c993e5
ikhbaaalll <ikhbal.madrid17@gmail.com>
2022-04-04 22:57:49 +0700

    change upload photo api to storage

--- api/app/Http/Controllers/Auth/RegisterController.php
+++ api/app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -20,8 +22,8 @@ class RegisterController extends Controller
             // 'photo' => ['required']
         ]);
 
-        if($request->photo) {
-            $photo = $this->uploadImage($request->photo);
+        if ($request->photo) {
+            $imageName = $this->upload($request->photo);
         }
 
         $user = User::create([
@@ -31,7 +33,7 @@ class RegisterController extends Controller
             'nik' => $request->nik,
             'phone' => $request->phone,
             'fcm_registration_id' => $request->fcm_registration_id,
-            'photo' => $photo
+            'photo' => "storage/images/ktp/{$imageName}"
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
@@ -39,5 +41,18 @@ class RegisterController extends Controller
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
+    }
+
+    public function upload($file)
+    {
+        $extension = explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
+        $replace = substr($file, 0, strpos($file, ',') + 1);
+        $image = str_replace($replace, '', $file);
+
+        $image = str_replace(' ', '+', $image);
+
+        $imageName = Str::random(40) . '.' . $extension;
+        Storage::disk('public')->put("images/ktp/{$imageName}", base64_decode($image));
+        return $imageName;
     }
 }
